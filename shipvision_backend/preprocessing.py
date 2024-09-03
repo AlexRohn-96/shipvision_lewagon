ce9a705abeb06687b1104c156dd96294bc648007
olivier picaud <picaud olivier>
2024-09-03 11:41:02 +0200

    update of transform function

--- shipvision_backend/preprocessing.py
+++ shipvision_backend/preprocessing.py
@@ -1,36 +1,7 @@
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.base import BaseEstimator, TransformerMixin
 
-class image_reshape_normalize((BaseEstimator, TransformerMixin)):
-    #create a class that inherits from BaseEstimator and TransformerMixin classes from scikit-learn.
-    def __init__(self,normalize= True):# Initialize with a parameter to control normalization
-        """
-        Initialize the transformer with optional parameters.
 
-        Parameters:
-        - normalize (bool): Whether to normalize the image pixel values to the range [0, 1].
-                            Default is True.
-        """
-        self.normalize=normalize
-    def fit(self, X,y=None):
-        """Fit method is required for the transformer but doesn't do anything in this case.
-        Purpose:
-        - This method is necessary to comply with scikit-learn's transformer interface.
-        - Even if no fitting is required, this method must return self to allow chaining
-          in pipelines.
-
-        Parameters:
-        - X (array-like): Input data (ignored in this method).
-        - y (array-like): Target data (optional, ignored in this method).
-
-        Returns:
-        - self: Returns the instance itself, allowing method chaining."""
-        return (self)
-
-    def transform(self,X):
-
+def transform(X,y=None):
         """  Apply the transformation to the input data.
         return a A NumPy array of reshaped and optionally normalized images,
         with shape (n_samples, 80, 80, 3)"""
