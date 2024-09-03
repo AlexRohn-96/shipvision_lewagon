3345632b0af3fccc38e1caa90130fbdfd7ffbc8a
Alexander Rohn <alexander.rohn@hec.edu>
2024-09-03 11:53:30 +0200

    Added basic registry.py module containing a function to save a model locally and a function to load a model. Also added a params.py file.

---
+++ shipvision_backend/params.py
@@ -0,0 +1,3 @@
+import os
+
+LOCAL_REGISTRY_PATH =  os.path.join('~', "code", "AlexRohn-96", "shipvision_lewagon_backend", "models")
---
+++ shipvision_backend/registry.py
@@ -0,0 +1,57 @@
+from tensorflow import keras
+import glob
+import os
+import time
+import pickle
+from params import *
+
+
+
+
+
+def save_model(model: keras.Model = None) -> None:
+    """
+    Save trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
+    """
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+
+
+    # Save model locally
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.h5")
+    model.save(model_path)
+
+    print("✅ Model saved locally")
+
+
+
+
+
+
+def load_model(stage="Production") -> keras.Model:
+    """
+    Return a saved model:
+    - locally (latest one in alphabetical order)
+
+    Return None (but do not Raise) if no model is found
+
+    """
+
+
+    # Get the latest model version name by the timestamp on disk
+    local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, "models")
+    local_model_paths = glob.glob(f"{local_model_directory}/*")
+
+    if not local_model_paths:
+        return None
+
+    most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
+
+    print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
+
+    latest_model = keras.models.load_model(most_recent_model_path_on_disk)
+
+    print("✅ Model loaded from local disk")
+
+    return latest_model
