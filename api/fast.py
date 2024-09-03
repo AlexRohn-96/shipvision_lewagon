1cdedfa7e3c34dfdb4e6a1cfafac1ff66b2acd72
Alexander Rohn <alexander.rohn@hec.edu>
2024-09-03 17:12:09 +0200

    API predict function modification

--- api/fast.py
+++ api/fast.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from shipvision_backend.preprocessing import *
+from shipvision_backend.registry import *
+from shipvision_backend.main import *
+from shipvision_backend.params import *
 
 app = FastAPI()
 
@@ -8,5 +12,13 @@ def index():
     return {'ok': True}
 
 @app.get("/predict")
-def predict():
-    return {'result': 1}
+def predict(X: list):
+
+    """
+    With this current version of predict, the input is a list of integers X containing the RGB pixel values of an image.
+    Based on that, we call the pred function from main.py to load the most recent model, preprocess X, and predict whether X is a ship or not.
+    """
+
+    y_pred = pred(X)
+
+    return {'Prediction': int(y_pred)}
